b80a92ed5338fd49a73225c6c8722e2e85313ecb
Rafael Elias Ioppi <141973836+RafaelEliasIoppi@users.noreply.github.com>
2024-03-19 23:27:04 +0000

    Rafael Elias Ioppi <

--- CPW1/Aula1903/AulaEnum/src/testes.java
+++ CPW1/Aula1903/AulaEnum/src/testes.java
@@ -1,14 +1,15 @@
 
 import javax.swing.JOptionPane;
-import java.awt.Menu;
 
 public class testes {
   
 
     public static void main(String[] args) {
         String aux = "";
-        for(Menu item : Menu.values()){
+        for(Menu item : Menu.values()) {
             aux += item.getOpcao()  + " - " + item.getDescricao() + "\n";
-        }
+              }
+              //javax.swing.JOptionPane.showMessageDialog(null, aux);
+              System.out.println(aux);
     }
 }
