a44223705180f0cb7a1d6f3d51c26552eb1aa088
Rafael Elias Ioppi <141973836+RafaelEliasIoppi@users.noreply.github.com>
2024-04-02 23:34:18 +0000

    Rafael Elias Ioppi <

--- CPW1/Aula5/Colecoes_Slides/src/conjuntos_slides/Conjunto1.java
+++ CPW1/Aula5/Colecoes_Slides/src/conjuntos_slides/Conjunto1.java
@@ -5,10 +5,13 @@ import java.util.*;
 public class Conjunto1 {
     public static void main(String[] args) {
         Set<String> conjunto = new HashSet<>();
+        // nao permite elemento duplicado
         conjunto.add("Dois");
         conjunto.add("Tres");
         conjunto.add("Um");
         conjunto.add("Um");
+        //numero de elementos do conjunto
+        System.out.println("Numero de elementos: " + conjunto.size());
         for (String num : conjunto) {
             System.out.println(num);
         }
