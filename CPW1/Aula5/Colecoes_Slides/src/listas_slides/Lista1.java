ef089a7b8a0e0bb5354a251d5a8bfe698aad6c73
Rafael Elias Ioppi <141973836+RafaelEliasIoppi@users.noreply.github.com>
2024-04-02 22:59:16 +0000

    rafael@rafael-Aspire-A515-51G:~/Documents/CPW1$ git commit

--- CPW1/Aula5/Colecoes_Slides/src/listas_slides/Lista1.java
+++ CPW1/Aula5/Colecoes_Slides/src/listas_slides/Lista1.java
@@ -9,15 +9,33 @@ import java.util.*;
 
 public class Lista1{
      public static void main(String args[]){
-            List<Integer> lista = new ArrayList<>();
+           // cria uma lista no formato de um vetor 
+           // ser usar number pode usar qualquer tipo de numero
+           List<Number> lista = new ArrayList<>();
+           // adiciona elementos na lista
             lista.add(10); 
             lista.add(20); 
+            lista.add(20L);// long
+            lista.add(20.5F);//float
+            lista.add(30.0);//double
+            lista.add(10);
             
-            for(Integer obj: lista) {
+            for(Number obj: lista) {
                     System.out.println(obj);
             }
-            System.out.println(lista.indexOf(20));
-     }
+            System.out.println("Retorna o indice da primeira ocorrencia do objeto 30.0 => " + lista.indexOf(30.0) + " posicao"); //retorna o indice da primeira ocorrencia do objeto
+            System.out.println("Retorna o indice da ultima ocorrencia do objeto 10 => " + lista.lastIndexOf(10) + " posicao"); 
+            System.out.println("Retorna o tamanho da lista => " + lista.size());
+            //remove o valor 20F da coleção
+            lista.remove(20F);
+           // remove o valor da posição 0
+            lista.remove(0);
+            
+            
+           
+
+
+     }      
      //int - Integer
      //char - Character
      //float - Float
